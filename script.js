712afeaf7525e8c9bd1871d016e3c9b38122bba0
Olaoluwa Akinfenwa <79768328+Hawsome@users.noreply.github.com>
2023-05-07 23:46:29 +0100

    A simple calculator that performs basic arithmetic operations

---
+++ script.js
@@ -0,0 +1,45 @@
+let display = document.querySelector(".display");
+let operator = "";
+let firstNumber = "";
+let secondNumber = "";
+
+function addToDisplay(num) {
+	if (display.value === "0") {
+		display.value = num;
+	} else {
+		display.value += num;
+	}
+}
+
+function clearDisplay() {
+	display.value = "0";
+	operator = "";
+	firstNumber = "";
+	secondNumber = "";
+}
+
+function setOperator(op) {
+	operator = op;
+	firstNumber = display.value;
+	display.value = "0";
+}
+
+function calculate() {
+	secondNumber = display.value;
+	let result = 0;
+
+	if (operator === "+") {
+		result = parseFloat(firstNumber) + parseFloat(secondNumber);
+	} else if (operator === "-") {
+		result = parseFloat(firstNumber) - parseFloat(secondNumber);
+	} else if (operator === "*") {
+		result = parseFloat(firstNumber) * parseFloat(secondNumber);
+	} else if (operator === "/") {
+		result = parseFloat(firstNumber) / parseFloat(secondNumber);
+	}
+
+	display.value = result;
+	operator = "";
+	firstNumber = "";
+	secondNumber = "";
+}
