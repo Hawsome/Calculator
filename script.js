9c57ded5e15b121f83691e0c2d7378dd730d1baa
Olaoluwa Akinfenwa <79768328+Hawsome@users.noreply.github.com>
2023-05-11 00:42:41 +0100

    Addition of delete function and input edit by keyboard

--- script.js
+++ script.js
@@ -43,104 +43,95 @@ function calculate() {
 	firstNumber = "";
 	secondNumber = "";
 }
+
+function deleteLastCharacter() {
+	display.value = display.value.slice(0, -1);
+  }
+
+function handleKeyboardInput(event) {
+	const key = event.key;
+	if (/\d/.test(key)) {
+		addToDisplay(key);
+	} else if (key === ".") {
+		addToDisplay(".");
+	} else if (key === "=" || key === "Enter") {
+		calculate();
+	} else if (key === "/") {
+		setOperator("/");
+	} else if (key === "*") {
+		setOperator("*");
+	} else if (key === "-") {
+		setOperator("-");
+	} else if (key === "+") {
+		setOperator("+");
+	}
+}
+
 document.addEventListener("keydown", function(event) {
 	switch (event.code) {
-	  case "Digit0":
-	  case "Numpad0":
-		addToDisplay("0");
-		break;
-	  case "Digit1":
-	  case "Numpad1":
-		addToDisplay("1");
-		break;
-	  case "Digit2":
-	  case "Numpad2":
-		addToDisplay("2");
-		break;
-	  case "Digit3":
-	  case "Numpad3":
-		addToDisplay("3");
-		break;
-	  case "Digit4":
-	  case "Numpad4":
-		addToDisplay("4");
-		break;
-	  case "Digit5":
-	  case "Numpad5":
-		addToDisplay("5");
-		break;
-	  case "Digit6":
-	  case "Numpad6":
-		addToDisplay("6");
-		break;
-	  case "Digit7":
-	  case "Numpad7":
-		addToDisplay("7");
-		break;
-	  case "Digit8":
-	  case "Numpad8":
-		addToDisplay("8");
-		break;
-	  case "Digit9":
-	  case "Numpad9":
-		addToDisplay("9");
-		break;
-	  case "Period":
-	  case "NumpadDecimal":
-		addToDisplay(".");
-		break;
-	  case "Equal":
-	  case "NumpadEnter":
-		calculate();
-		break;
-	  case "Slash":
-	  case "NumpadDivide":
-		setOperator("/");
-		break;
-	  case "Asterisk":
-	  case "NumpadMultiply":
-		setOperator("*");
-		break;
-	  case "Minus":
-	  case "NumpadSubtract":
-		setOperator("-");
-		break;
-	  case "Plus":
-	  case "NumpadAdd":
-		setOperator("+");
-		break;
+		case "Digit0":
+		case "Numpad0":
+			addToDisplay("0");
+			break;
+		case "Digit1":
+		case "Numpad1":
+			addToDisplay("1");
+			break;
+		case "Digit2":
+		case "Numpad2":
+			addToDisplay("2");
+			break;
+		case "Digit3":
+		case "Numpad3":
+			addToDisplay("3");
+			break;
+		case "Digit4":
+		case "Numpad4":
+			addToDisplay("4");
+			break;
+		case "Digit5":
+		case "Numpad5":
+			addToDisplay("5");
+			break;
+		case "Digit6":
+		case "Numpad6":
+			addToDisplay("6");
+			break;
+		case "Digit7":
+		case "Numpad7":
+			addToDisplay("7");
+			break;
+		case "Digit8":
+		case "Numpad8":
+			addToDisplay("8");
+			break;
+		case "Digit9":
+		case "Numpad9":
+			addToDisplay("9");
+			break;
+		case "Period":
+		case "NumpadDecimal":
+			addToDisplay(".");
+			break;
+		case "Equal":
+		case "NumpadEnter":
+			calculate();
+			break;
+		case "Slash":
+		case "NumpadDivide":
+			setOperator("/");
+			break;
+		case "Asterisk":
+		case "NumpadMultiply":
+			setOperator("*");
+			break;
+		case "Minus":
+		case "NumpadSubtract":
+			setOperator("-");
+			break;
+		case "Plus":
+		case "NumpadAdd":
+			setOperator("+");
+			break;
 	}
-  });
-  function setOperator(op) {
-	if (isNaN(parseFloat(display.value))) {
-	  console.error("Invalid input for firstNumber: " + display.value);
-	  return;
-	}
-	operator = op;
-	firstNumber = display.value;
-	display.value = "0";
-  }
-  
-  function calculate() {
-	if (isNaN(parseFloat(firstNumber)) || isNaN(parseFloat(display.value))) {
-	  console.error("Invalid input for calculation: " + firstNumber + ", " + display.value);
-	  return;
-	}
-	secondNumber = display.value;
-	let result = 0;
-  
-	if (operator === "+") {
-	  result = parseFloat(firstNumber) + parseFloat(secondNumber);
-	} else if (operator === "-") {
-	  result = parseFloat(firstNumber) - parseFloat(secondNumber);
-	} else if (operator === "*") {
-	  result = parseFloat(firstNumber) * parseFloat(secondNumber);
-	} else if (operator === "/") {
-	  result = parseFloat(firstNumber) / parseFloat(secondNumber);
-	}
-  
-	display.value = result;
-	operator = "";
-	firstNumber = "";
-	secondNumber = "";
-  }
+});
